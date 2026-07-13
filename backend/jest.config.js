49462fd230058329586a870943b959f387df9969
asad132509-dev <asad132509@gmail.com>
2026-07-13 20:31:07 +0500

    Add: Backend Configuration Files

---
+++ backend/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.spec.ts',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>', '<rootDir>/../test'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@config/(.*)$': '<rootDir>/config/$1',
+    '^@common/(.*)$': '<rootDir>/common/$1',
+    '^@modules/(.*)$': '<rootDir>/modules/$1',
+    '^@database/(.*)$': '<rootDir>/database/$1',
+    '^@utils/(.*)$': '<rootDir>/utils/$1',
+  },
+};
